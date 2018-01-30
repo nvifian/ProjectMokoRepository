caed2746e4c4429298409b3d49159646444bf510
nvifian <nvifian@gmail.com>
2018-01-29 21:51:26 -0800

    1-29 Update

---
+++ echoProject/Assets/Scripts/dolphinProgressScript.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dolphinProgressScript : MonoBehaviour
+{
+	public static int questionInt;
+	public Transform dolphinStickTransform;
+	public Transform dot1a;
+	public Transform dot1b;
+	public Transform dot1c;
+	public Transform dot2a;
+	public Transform dot2b;
+	public Transform dot2c;
+	public Transform dot2d;
+	public Transform dot3a;
+	public Transform dot3b;
+	public Transform dot3c;
+	public Transform dot3d;
+	public Transform dot4a;
+	public Transform dot4b;
+	public Transform dot4c;
+	public Transform dot4d;
+	public Transform dot5a;
+
+	void Update ()
+	{
+		Debug.Log (questionInt);
+		if (fishScript.questionInt == 0)
+		{
+			dolphinStickTransform.position = dot1a.position;
+		}
+		if (fishScript.questionInt == 1)
+		{
+			dolphinStickTransform.position = dot1a.position;
+		}
+		if (fishScript.questionInt == 2)
+		{
+			dolphinStickTransform.position = dot1a.position;
+		}
+		if (fishScript.questionInt == 3)
+		{
+			dolphinStickTransform.position = dot1b.position;
+		}
+		if (fishScript.questionInt == 4)
+		{
+			dolphinStickTransform.position = dot1b.position;
+		}
+		if (fishScript.questionInt == 5)
+		{
+			dolphinStickTransform.position = dot1b.position;
+		}
+		if (fishScript.questionInt == 6)
+		{
+			dolphinStickTransform.position = dot1c.position;
+		}
+		if (fishScript.questionInt == 7)
+		{
+			dolphinStickTransform.position = dot1c.position;
+		}
+		if (fishScript.questionInt == 8)
+		{
+			dolphinStickTransform.position = dot1c.position;
+		}
+		if (fishScript.questionInt == 9)
+		{
+			dolphinStickTransform.position = dot1c.position;
+		}
+		if (fishScript.questionInt == 10)
+		{
+			dolphinStickTransform.position = dot2a.position;
+		}
+		if (fishScript.questionInt == 11)
+		{
+			dolphinStickTransform.position = dot2a.position;
+		}
+		if (fishScript.questionInt == 12)
+		{
+			dolphinStickTransform.position = dot2b.position;
+		}
+		if (fishScript.questionInt == 13)
+		{
+			dolphinStickTransform.position = dot2b.position;
+		}
+		if (fishScript.questionInt == 14)
+		{
+			dolphinStickTransform.position = dot2b.position;
+		}
+		if (fishScript.questionInt == 15)
+		{
+			dolphinStickTransform.position = dot2c.position;
+		}		
+		if (fishScript.questionInt == 16)
+		{
+			dolphinStickTransform.position = dot2c.position;
+		}
+		if (fishScript.questionInt == 17)
+		{
+			dolphinStickTransform.position = dot2c.position;
+		}
+		if (fishScript.questionInt == 18)
+		{
+			dolphinStickTransform.position = dot2d.position;
+		}
+		if (fishScript.questionInt == 19)
+		{
+			dolphinStickTransform.position = dot2d.position;
+		}
+		if (fishScript.questionInt == 20)
+		{
+			dolphinStickTransform.position = dot2d.position;
+		}
+		if (fishScript.questionInt == 21)
+		{
+			dolphinStickTransform.position = dot3a.position;
+		}
+		if (fishScript.questionInt == 22)
+		{
+			dolphinStickTransform.position = dot3a.position;
+		}
+		if (fishScript.questionInt == 23)
+		{
+			dolphinStickTransform.position = dot3a.position;
+		}
+		if (fishScript.questionInt == 24)
+		{
+			dolphinStickTransform.position = dot3a.position;
+		}
+		if (fishScript.questionInt == 25)
+		{
+			dolphinStickTransform.position = dot3b.position;
+		}
+		if (fishScript.questionInt == 26)
+		{
+			dolphinStickTransform.position = dot3b.position;
+		}
+		if (fishScript.questionInt == 27)
+		{
+			dolphinStickTransform.position = dot3b.position;
+		}
+		if (fishScript.questionInt == 28)
+		{
+			dolphinStickTransform.position = dot3b.position;
+		}
+		if (fishScript.questionInt == 29)
+		{
+			dolphinStickTransform.position = dot3c.position;
+		}
+		if (fishScript.questionInt == 30)
+		{
+			dolphinStickTransform.position = dot3c.position;
+		}
+		if (fishScript.questionInt == 31)
+		{
+			dolphinStickTransform.position = dot3c.position;
+		}
+		if (fishScript.questionInt == 32)
+		{
+			dolphinStickTransform.position = dot3d.position;
+		}
+		if (fishScript.questionInt == 33)
+		{
+			dolphinStickTransform.position = dot3d.position;
+		}
+		if (fishScript.questionInt == 34)
+		{
+			dolphinStickTransform.position = dot3d.position;
+		}
+		if (fishScript.questionInt == 35)
+		{
+			dolphinStickTransform.position = dot4a.position;
+		}
+		if (fishScript.questionInt == 36)
+		{
+			dolphinStickTransform.position = dot4a.position;
+		}
+		if (fishScript.questionInt == 37)
+		{
+			dolphinStickTransform.position = dot4a.position;
+		}
+		if (fishScript.questionInt == 38)
+		{
+			dolphinStickTransform.position = dot4a.position;
+		}
+		if (fishScript.questionInt == 39)
+		{
+			dolphinStickTransform.position = dot4b.position;
+		}
+		if (fishScript.questionInt == 40)
+		{
+			dolphinStickTransform.position = dot4b.position;
+		}
+		if (fishScript.questionInt == 41)
+		{
+			dolphinStickTransform.position = dot4b.position;
+		}
+		if (fishScript.questionInt == 42)
+		{
+			dolphinStickTransform.position = dot4b.position;
+		}
+		if (fishScript.questionInt == 43)
+		{
+			dolphinStickTransform.position = dot4c.position;
+		}
+		if (fishScript.questionInt == 44)
+		{
+			dolphinStickTransform.position = dot4c.position;
+		}
+		if (fishScript.questionInt == 45)
+		{
+			dolphinStickTransform.position = dot4c.position;
+		}
+		if (fishScript.questionInt == 46)
+		{
+			dolphinStickTransform.position = dot4c.position;
+		}
+		if (fishScript.questionInt == 47)
+		{
+			dolphinStickTransform.position = dot4d.position;
+		}
+		if (fishScript.questionInt == 48)
+		{
+			dolphinStickTransform.position = dot4d.position;
+		}
+		if (fishScript.questionInt == 49)
+		{
+			dolphinStickTransform.position = dot4d.position;
+		}
+		if (fishScript.questionInt == 50)
+		{
+			dolphinStickTransform.position = dot5a.position;
+		}
+	}
+}
---
+++ echoProject/Assets/Scripts/mainMenuXpScript.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class mainMenuXpScript : MonoBehaviour
+{
+	public static int lvlInt;
+	public static bool lvl1Bool;
+	public static bool lvl2Bool;
+	public static bool lvl3Bool;
+	public static bool lvl4Bool;
+	public static bool lvl5Bool;
+	public static int questionInt;
+	public static GameObject lvl5Object;
+	public GameObject xp1Object;
+	public GameObject xp2Object;
+	public GameObject xp3Object;
+	public GameObject xp4Object;
+	public GameObject xp5Object;
+	public GameObject xp6Object;
+	public GameObject xp7Object;
+	public GameObject xp8Object;
+	public GameObject xp9Object;
+	public GameObject xp10Object;
+	public GameObject xp11Object;
+	public GameObject xp12Object;
+	public GameObject xp13Object;
+	public GameObject xp14Object;
+	public GameObject xp15Object;
+	public GameObject xp16Object;
+	public GameObject xp17Object;
+	public GameObject xp18Object;
+	public GameObject xp19Object;
+	public GameObject xp20Object;
+	public Text lvlText; //use this to display level, change to 'level up!' when level change
+	public Text xpText;
+
+	public void Update ()
+	{
+		if (fishScript.lvlInt == 1)
+		{
+			lvlText.text = "Level 1";
+		}
+		if (fishScript.lvlInt == 2)
+		{
+			lvlText.text = "Level 2";
+		}
+		if (fishScript.lvlInt == 3)
+		{
+			lvlText.text = "Level 3";
+		}
+		if (fishScript.lvlInt == 4)
+		{
+			lvlText.text = "Level 4";
+		}
+		if (fishScript.lvlInt == 5)
+		{
+			lvlText.text = "Level 5";
+			buttonScript.lvl5Object.SetActive (true);
+		}
+
+		if (questionInt == 1)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp1Object.SetActive (true);
+		}
+		if (questionInt == 2)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp2Object.SetActive (true);
+		}
+		if (questionInt == 3)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp3Object.SetActive (true);
+		}
+		if (questionInt == 4)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp4Object.SetActive (true);
+		}
+		if (questionInt == 5)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp5Object.SetActive (true);
+		}
+		if (questionInt == 6)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp6Object.SetActive (true);
+		}
+		if (questionInt == 7)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp7Object.SetActive (true);
+		}
+		if (questionInt == 8)
+		{
+			xpText.text = questionInt + "  /  9";
+			xp8Object.SetActive (true);
+		}
+		if (questionInt == 9)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp10Object.SetActive (true);
+			xp1Object.SetActive (false);
+			xp2Object.SetActive (false);
+			xp3Object.SetActive (false);
+			xp4Object.SetActive (false);
+			xp5Object.SetActive (false);
+			xp6Object.SetActive (false);
+			xp7Object.SetActive (false);
+			xp8Object.SetActive (false);
+			xp9Object.SetActive (false);
+		}
+		//LVL 2 // CHANGE questionGroupText string
+		if (questionInt == 10)
+		{
+			lvlInt = 2;
+			xpText.text = questionInt-9 + "  /  11";
+			xp10Object.SetActive (true);
+		}
+		if (questionInt == 11)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp11Object.SetActive (true);
+		}
+		if (questionInt == 12)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp12Object.SetActive (true);
+		}
+		if (questionInt == 13)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp13Object.SetActive (true);
+		}
+		if (questionInt == 14)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp14Object.SetActive (true);
+		}
+		if (questionInt == 15)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp15Object.SetActive (true);
+		}
+		if (questionInt == 16)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp16Object.SetActive (true);
+		}
+		if (questionInt == 17)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp17Object.SetActive (true);
+		}
+		if (questionInt == 18)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp18Object.SetActive (true);
+		}
+		if (questionInt ==19)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp19Object.SetActive (true);
+		}
+		if (questionInt == 20)
+		{
+			xpText.text = questionInt-9 + "  /  11";
+			xp20Object.SetActive (true);
+			xpText.text = "NEW ITEM!";
+		}
+		//LVL3
+		if (questionInt == 21)
+		{
+			xpText.text = questionInt-20 + "  /  14";
+		}
+		if (questionInt == 22)
+		{
+			xpText.text = questionInt-20 + "  /  14";
+		}
+
+	}
+}
